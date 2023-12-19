0e20a0d0b51ae0b8c2680da01861be397447ae8b
FHDavies <f.h.davies2@exeter.ac.uk>
2023-12-19 16:03:24 +0000

    made changes to utl codes

--- utl/geom-grid3d.py
+++ utl/geom-grid3d.py
@@ -1,4 +1,4 @@
-#!/home/links/fhd205/.conda/envs/py3.7.6/bin/python
+#!/home/fhd205/opt/anaconda3/envs/py3.11/bin/python
 import sys
 import math
 import numpy as np
@@ -81,41 +81,6 @@ def cylind_read(block_type, line, block_data):
         if line_lower.startswith("fill"):
             key, value = multi_split(line_lower, '=', ':')
             block_data[key.strip()] = str(value.strip())
-
-vert = False
-face = False
-fi = 0
-def struct_read(block_type, line, block_data):
-    global vert, face, vi, fi
-    if block_type == "STRUCTURE":
-        #print('Here')
-        line_lower = line.lower()
-        if line_lower.startswith("fill"):
-            key, value = multi_split(line_lower, '=', ':')
-            block_data[key.strip()] = str(value.strip())
-        elif line == "VERTICES":
-            block_data["VERTICES"] = []
-            vert = True
-            face = False
-        elif line == "FACES":
-            block_data["FACES"] = []
-            face = True
-            vert = False
-        else:
-            if vert:
-                vertex_data = {}
-                key, value = multi_split(line_lower, '=', ':')
-                v1, v2, v3 = value.split()
-                vertex_data[key.strip()] = [float(v1), float(v2), float(v3)]
-                block_data["VERTICES"].append(vertex_data)
-            if face:
-                key, value = multi_split(line_lower, '=', ':')
-                vertices = value.split()
-                for i in range(1, len(vertices) - 1):
-                    face_data = {}
-                    fi=fi+1
-                    face_data[str(fi)] = [int(vertices[0]), int(vertices[i]), int(vertices[i+1])]
-                    block_data["FACES"].append(face_data)
 ###############################################################################
 
 
@@ -141,7 +106,6 @@ def read_infile(filename):
                 cuboid_read(cur_block, line, cur_block_data)
                 sphere_read(cur_block, line, cur_block_data)
                 cylind_read(cur_block, line, cur_block_data)
-                struct_read(cur_block, line, cur_block_data)
     return blocks
 
 
@@ -161,103 +125,6 @@ def write_data(blocks):
     print("---------")
 
 ###############################################################################
-
-
-###############################################################################
-# code to determin if shapes defined by the STRUCTURE block are sealed surfaces
-###############################################################################
-def get_edge(vertex1, vertex2):
-    #Return a tuple representing an edge, ensuring a consistent ordering.
-    return tuple(sorted([vertex1, vertex2]))
-
-def is_closed_surface(structure):
-    # each edge can only be met by 2 faces
-    edge_count = {}
-    # Count edges for all faces
-    for face in structure['FACES']:
-        vertices = list(face.values())[0]
-        edge = get_edge(vertices[0], vertices[1])
-        edge_count[edge] = edge_count.get(edge, 0) + 1
-        edge = get_edge(vertices[0], vertices[2])
-        edge_count[edge] = edge_count.get(edge, 0) + 1
-        edge = get_edge(vertices[1], vertices[2])
-        edge_count[edge] = edge_count.get(edge, 0) + 1
-    # Check if every edge is used exactly twice
-    for count in edge_count.values():
-        if count != 2:
-            return False
-    return True
-
-###############################################################################
-
-
-###############################################################################
-# functions to determin if a coordinate falls with in any defined volume
-###############################################################################
-def plane_intersection(ray_origin, triangle_vertices):
-    # writing this was absolute hell
-    # it's now working but i don't know what is different ...
-    # ... that makes this work and preveous ones not work!
-    ray_vector = np.array([1.0, 0.0, 0.0])
-    EPSILON = 1e-7
-    vertex_A, vertex_B, vertex_C = triangle_vertices
-    
-    edge_AB = vertex_B - vertex_A
-    edge_AC = vertex_C - vertex_A
-    orthogonal_vector = np.cross(ray_vector, edge_AC)
-    dot_product = np.dot(edge_AB, orthogonal_vector)
-    
-    if -EPSILON < dot_product < EPSILON:
-        return False, None  # This ray is parallel to this triangle.
-    
-    inverse_dot = 1.0 / dot_product
-    s_vector = ray_origin - vertex_A
-    param_u = inverse_dot * np.dot(s_vector, orthogonal_vector)
-    
-    if param_u < 0.0 or param_u > 1.0:
-        return False, None
-    
-    q_vector = np.cross(s_vector, edge_AB)
-    param_v = inverse_dot * np.dot(ray_vector, q_vector)
-    
-    if param_v < 0.0 or param_u + param_v > 1.0:
-        return False, None
-    
-    # Compute t to find the intersection point on the line.
-    t_param = inverse_dot * np.dot(edge_AC, q_vector)
-    
-    if t_param > EPSILON:  # ray intersection
-        intersection_point = ray_origin + ray_vector * t_param
-        return True, intersection_point
-    else:  # Line intersection but not a ray intersection.
-        return False, None
-
-
-def point_in_poly(origin,struct):
-    intersection_count = 0
-    for face_entry in struct['FACES']:
-        face = list(face_entry.values())[0]
-        vertices = [np.array(list(struct['VERTICES'][v-1].values())[0]) for v in face]
-        intersects, intersection_location = plane_intersection(origin, vertices)
-        if intersects:
-            intersection_count += 1
-    return intersection_count
-
-
-def point_in_structures(point,Structure):
-    origin = np.array(point["coord"])
-    material = point["fill"]
-    count=0
-    for struct in Structure:
-        count=point_in_poly(origin,struct)
-        if count % 2 == 0:
-            if verbose:
-                print("Point is outside the structure.",point["fill"])
-        else:
-            point["fill"]=struct["fill"]
-            if verbose:
-                print("Point is inside the structure.",point["fill"])
-
 
 def point_in_cube(cord,struct):
     origin=np.array(struct["origin"])
@@ -315,10 +182,11 @@ def point_in_cylinder(cord,struct):
     radius=np.array(struct["radius"])
     length=np.array(struct["length"])
     direction=np.array(struct["direction"])
+    direction = direction / np.linalg.norm(direction)
 
     h = cord-start
-    dist_to_line = abs(np.dot(direction,h))
-    dist_on_line = np.dot(np.cross(direction,np.cross(direction,h)),direction)
+    dist_on_line = np.dot(direction,h)
+    dist_to_line = np.sqrt(np.dot(h,h)-np.dot(direction,h)**2)
     if 0 <= dist_on_line <= length and dist_to_line <= radius:
         return True
     else:
@@ -342,17 +210,17 @@ def point_in_cylinders(point,Structure):
 
 
 def set_points_material(point,data_structure):
-    struct=data_structure['STRUCTURE']
-    point_in_structures(point,struct)
+    if 'CUBOID' in data_structure:
+        cube=data_structure['CUBOID']
+        point_in_cuboids(point,cube)
     
-    cube=data_structure['CUBOID']
-    point_in_cuboids(point,cube)
+    if 'SPHERE' in data_structure:
+        sphere=data_structure['SPHERE']
+        point_in_spheres(point,sphere)
     
-    sphere=data_structure['SPHERE']
-    point_in_spheres(point,sphere)
-    
-    cylinder=data_structure['CYLINDER']
-    point_in_cylinders(point,cylinder)
+    if 'CYLINDER' in data_structure:
+        cylinder=data_structure['CYLINDER']
+        point_in_cylinders(point,cylinder)
 
 ###############################################################################
 
@@ -365,6 +233,8 @@ def create_grid(X, Y, Z, grid__dims):
     dx = X / (grid_dims[0])
     dy = Y / (grid_dims[1])
     dz = Z / (grid_dims[2])
+    
+    #print("dx ="+str(X)+"/"+str(grid_dims[0])+" = "+str(dx))
 
     grid_points = []
 
@@ -382,6 +252,7 @@ def create_grid(X, Y, Z, grid__dims):
                     "coord": [x_coord, y_coord, z_coord],
                     "fill":  blocks["VOLUME"][0]["fill"]
                 }
+                #print(point["coord"])
                 row.append(point)
 
             layer.append(row)
@@ -411,18 +282,11 @@ verbose=False
 # read the input file 
 filename = sys.argv[1]
 blocks = read_infile(filename)
+print(blocks)
 
 
 # uncoment line to output the input (dev test)
 write_data(blocks);print("")
-
-
-# checks if defined structures form a closed surface
-for c, structure in enumerate(blocks['STRUCTURE']):
-    if not is_closed_surface(structure):
-        print("Error: Structure {} is not closed!".format(c+1))
-        exit(1)
-print(" All structures are closed!")
 
 
 # ceate the point mesh. each coordinate is at the center of it's respective cell
@@ -431,7 +295,7 @@ Y = blocks["VOLUME"][0]["y"]
 Z = blocks["VOLUME"][0]["z"]
 grid_dims = blocks["VOLUME"][0]["grid"]
 point_mesh = create_grid(X, Y, Z, grid_dims)
-
+print("mesh built");print("")
 
 # assine a material to each cell/point
 i=int(0)
@@ -440,13 +304,13 @@ k=int(0)
 for i in range(grid_dims[0]):
     for j in range(grid_dims[1]):
         for k in range(grid_dims[2]):
-            point=point_mesh[k][j][i]
-            set_points_material(point,blocks)
-            coordinate = point_mesh[k][j][i]['coord']
-            fill = point_mesh[k][j][i]['fill']
+            point=point_mesh[i][j][k] 
+            set_points_material(point,blocks) 
+            coordinate = point['coord']
+            fill = point['fill'] 
 
 
-print(" Writing to: GridMaterial.dat")
+print(" Writing to: geom.in")
 with open("geom.in", "w") as f:
     s=blocks["VOLUME"][0]["units"]
     x=str(blocks["VOLUME"][0]["x"]*s)
@@ -455,34 +319,20 @@ with open("geom.in", "w") as f:
     i=str(grid_dims[0])
     j=str(grid_dims[1])
     k=str(grid_dims[2])
-    f.write(x+' '+y+' '+z+'\n')
-    f.write(i+' '+j+' '+k+'\n\n')
+    f.write(i+' '+j+' '+k+'\n')
+    f.write(x+' '+y+' '+z+'\n\n')
     i=int(0)
     j=int(0)
     k=int(0)
-    for i in range(grid_dims[0]):
+    for k in range(grid_dims[2]):
         for j in range(grid_dims[1]):
             line_values = []
-            for k in range(grid_dims[2]):
-                coordinate = point_mesh[k][j][i]['coord']
-                fill = int(point_mesh[k][j][i]['fill'])
+            for i in range(grid_dims[0]):
+                #coordinate = point_mesh[i][j][k]['coord']
+                fill = int(point_mesh[i][j][k]['fill'])
                 line_values.append(str(fill))
             f.write(" ".join(line_values) + "\n")
         f.write("\n")
-
-#with open("GridMaterial.bin", "wb") as f:
-#    i=int(0)
-#    j=int(0)
-#    k=int(0)
-#    for i in range(grid_dims[0]):
-#        for j in range(grid_dims[1]):
-#            line_values = []
-#            for k in range(grid_dims[2]):
-#                coordinate = point_mesh[k][j][i]['coord']
-#                fill = int(point_mesh[k][j][i]['fill'])
-#                line_values.append(str(fill))
-#                f.write(struct.pack('i', fill))
-
 
 
 # print out an input file in a readable format for HEATFOLW
