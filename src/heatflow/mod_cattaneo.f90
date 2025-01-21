273c48a8514cbce104f19223ed8eb38e2085b848
hm556 <hm556@exeter.ac.uk>
2025-01-21 21:05:18 +0000

    remove pure from decluration

--- src/heatflow/mod_cattaneo.f90
+++ src/heatflow/mod_cattaneo.f90
@@ -20,7 +20,7 @@ contains
 !!! - S_cat, This is the Cattaneo term for the S vector.
 !!!##############################################################################################
 
-pure subroutine S_catS(s_cat)
+subroutine S_catS(s_cat)
     real(real12), dimension(NA), intent(inout) :: S_cat
     integer(int12) :: index, ix, iy, iz
 
