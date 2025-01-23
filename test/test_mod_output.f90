f39de2cae41903ccec4230f30085e981299ce026
hm556 <hm556@exeter.ac.uk>
2025-01-23 11:55:54 +0000

    Fix test, now working

--- test/test_mod_output.f90
+++ test/test_mod_output.f90
@@ -1,16 +1,17 @@
+
 program test_mod_output
-    use output, only: last_log, data_write
-    use constants, only: real12, int12
-    use inputs, only: nx, ny, nz, NA, time_step, Test_Run, Check_Steady_State, &
-              write_every, WriteToTxt, ntime, freq, RunName, IVERB, start_ix, end_ix, &
-                start_iy, end_iy, start_iz, end_iz
-    use globe_data, only: Temp_p, Temp_pp, heat, heated_volume
+    use output, only: last_log
+    use constants, only: real12, int12, TINY, fields
+    use inputs, only: nx,ny,nz, time_step, grid, NA, Check_Steady_State, ntime, WriteToTxt
+    use inputs, only: Test_Run, freq, RunName, FullRestart, IVERB, write_every
+    use inputs, only: start_ix, end_ix, start_iy, end_iy, start_iz, end_iz
+    use globe_data, only: Temp_p,Temp_pp, heat, heated_volume
     implicit none
 
     ! Test variables
-    integer(int12) :: test_itime
-    character(len=1024) :: test_logname, test_outdir
-    logical :: file_exists
+    integer(int12) :: test_itime, newunit
+    character(len=1024) :: test_logname, test_outdir, cwdstring, full_path
+    logical :: file_exists, success
 
     ! Initialize test values
     nx = 2
@@ -23,7 +24,7 @@ program test_mod_output
     write_every = 1
     WriteToTxt = .true.
     ntime = 10
-    freq = 1.0
+    freq = 0.0
     RunName = "test"
     IVERB = 4
     
@@ -41,37 +42,51 @@ program test_mod_output
     heat = 100.0_real12
     heated_volume = 1.0_real12
 
+    success = .true.
+
     ! Test last_log subroutine
-    test_outdir = "./test_outputs/"
+    call getcwd(cwdstring)
+    write(*,*) "Current working directory: ", trim(cwdstring)
+    test_outdir = "/test_outputs/"
+    test_outdir = trim(adjustl(test_outdir))
+    print *, "Test outdir: ", test_outdir
+    cwdstring = trim(adjustl(cwdstring))
+    full_path = trim(cwdstring) // trim(test_outdir)
+    test_outdir = full_path
     call system("mkdir -p " // trim(test_outdir))
-    call last_log(test_logname, test_outdir)
-    inquire(file=test_logname, exist=file_exists)
+    
+    !Check directory creation
+    inquire(file=test_outdir, exist=file_exists)
     if (.not. file_exists) then
-        write(*,*) "Test last_log: FAILED"
+        write(0,*) "Test last_log, dir creation: FAILED"
+        success = .false.
+    else
+        write(*,*) "Test last_log, dir creation: PASSED"
+    endif
+
+    !Check file creation
+
+    call last_log(test_logname, test_outdir)
+    print *, "Test logname: ", test_logname
+    if (test_logname == "") then
+        write(0,*) "Test last_log: FAILED"
+        success = .false.
     else
         write(*,*) "Test last_log: PASSED"
-        stop 1  
     endif
 
-    ! Test data_write subroutine
-    test_itime = 1
-    call system("mkdir -p ./outputs/")
-    call data_write(test_itime)
-    inquire(file="./outputs/TempDis.dat", exist=file_exists)
-    if (file_exists) then
-        write(*,*) "Test data_write outputs: PASSED"
-    else
-        write(*,*) "Test data_write outputs: FAILED"
-        stop 1
-    endif
 
     ! Cleanup
-    call system("rm -rf ./test_outputs/")
-    call system("rm -rf ./outputs/*")
-    call system("rm -rf ./outputs/")
+    call system("rm -rf " // trim(test_outdir))
+
     deallocate(Temp_p)
     deallocate(Temp_pp)
 
-    write(*,*) "PASS: test_mod_output"
+    if (success) then
+        write(*,*) "PASS: test_mod_output"
+    else
+        write(0,*) "FAIL: test_mod_output"
+        stop 1 
+    endif
 
 end program test_mod_output
