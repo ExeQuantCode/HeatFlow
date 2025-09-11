2c996d8c20704b10c14bb5b7e561e8590589fba3
Your Name <hm556@exeter.ac.uk>
2025-09-11 13:53:34 +0100

    Fix solving

--- src/heatflow/mod_evolve.f90
+++ src/heatflow/mod_evolve.f90
@@ -170,7 +170,7 @@ contains
     allocate(acsr(jac%len), ja(jac%len), ia(jac%n+1))
 
     CALL  coo2csr(jac%n, jac%len, jac%val, jac%irow, jac%jcol, acsr, ja, ia)
-    CALL solve_pardiso(acsr, F, ia, ja, delta)
+    CALL solve_pardiso(acsr, -1.0_real12*F(:), ia, ja, delta)
     
     deallocate(acsr, ja, ia)
     
